ed297b16a3fb9332f7de78307baab614e7e97c5c
wuzhitian <linewu@yeah.net>
2015-06-24 01:42:22 +0800

    init
    git init

---
+++ 01/test1.java
@@ -0,0 +1,11 @@
+/*演示程序
+演示程序
+演示程序*/
+class test1         //class:关键字，demo:类名
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello 爪哇");
+		System.out.println("hello hello");
+	}
+}

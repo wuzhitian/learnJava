9ed12b286cafce9ea6feb8a584b080ddcb027967
zhitian <linewu@yeah.net>
2015-06-24 18:16:43 +0800

    变量
    变量介绍

---
+++ 02/demo1.java
@@ -0,0 +1,8 @@
+class demo1
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Integer.toBinaryString(-6));
+		System.out.println(0x3c);
+	}
+}
---
+++ 02/demo2.java
@@ -0,0 +1,10 @@
+class demo2
+{
+	public static void main(String[] args)
+	{
+		//定义变量的格式
+		//数据类型 变量名 = 初始值;
+		byte age = 14;
+		System.out.println(age);
+	}
+}

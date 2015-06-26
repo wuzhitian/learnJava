3ac1418d2636df948719893386ef168735beeb02
zhitian <linewu@yeah.net>
2015-06-26 18:04:33 +0800

    switchDay0

---
+++ 02/switchDay.java
@@ -0,0 +1,32 @@
+class switchDay
+{
+	public static void main(String[] args)
+	{
+		int  x = 4;
+		switch(x)
+		{
+			case 3:
+			case 4:
+			case 5:
+				System.out.println("a");
+				break;
+			case 6:
+			case 7:
+			case 8:
+				System.out.println("a");
+				break;
+			case 9:
+			case 10:
+			case 11:
+				System.out.println("a");
+				break;
+			case 12:
+			case 1:
+			case 2:
+				System.out.println("a");
+				break;
+			default:
+				System.out.println("NONE");
+		}
+	}
+}

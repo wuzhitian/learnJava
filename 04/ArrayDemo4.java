2832f43662502ff70e37bcf524312f76bb8b6fd6
wuzhitian <linewu@yeah.net>
2015-07-02 00:50:30 +0800

    toHex();
    toHex();

--- 04/ArrayDemo4.java
+++ 04/ArrayDemo4.java
@@ -4,6 +4,7 @@ class ArrayDemo4
 	{
 		System.out.println("Hello");
 		toBin(28);
+		toHex(60);
 	}
 	/*十进制-->二进制*/
 	public static void toBin(int num)
@@ -13,6 +14,26 @@ class ArrayDemo4
 		{
 			sb.append(num % 2);
 			num = num / 2;
+		}
+		System.out.println(sb.reverse());
+	}
+	/*十进制-->十六进制*/
+	public static void toHex(int num)
+	{
+		StringBuffer sb =new StringBuffer();
+		for(int x = 0; x < 8; x++)
+		//int字节长度为32位，每次位移4位，所以最多位移8次
+		{
+			int temp = num & 15;
+			if(temp > 9)
+			{
+				sb.append((char)(temp-10+'A'));
+			}
+			else
+			{
+				sb.append(temp);
+			}
+			num = num >> 4;
 		}
 		System.out.println(sb.reverse());
 	}
